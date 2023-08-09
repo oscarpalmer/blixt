69b9077c72c789b1d8849c1ab52986ed0c73969c
Oscar Palmér <opalmer@me.com>
2023-08-09 16:07:56 +0200

    0.6.1 - removed internal types

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,12 +4,14 @@
  * @returns {boolean}
  */
 export function isStore(value: any): boolean;
+
 /**
  * Creates a reactive store
  * @param {object} data
  * @returns {object}
  */
 export function store(data: object): object;
+
 /**
  * Subscribes to value changes for a key in a store
  * @param {any} store
@@ -18,6 +20,7 @@ export function store(data: object): object;
  * @returns {void}
  */
 export function subscribe(store: any, key: Key, callback: Subscriber): void;
+
 /**
  * Unsibscribes from value changes for a key in a store
  * @param {any} store
@@ -25,14 +28,9 @@ export function subscribe(store: any, key: Key, callback: Subscriber): void;
  * @param {Subscriber} callback
  */
 export function unsubscribe(store: any, key: Key, callback: Subscriber): void;
-export type ArrayParameters = {
-	array: any[];
-	callback: string;
-	state: State;
-	prefix: string;
-	value: any;
-};
+
 export type Key = number | string | symbol;
+
 export type Subscriber = (value: any, origin?: string) => void;
-export class State {}
+
 export const name: 'template';
